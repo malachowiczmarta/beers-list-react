1588257b637d55114b20ce93973fdb7b38d9bae6
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-01-19 11:18:57 +0100

    working beer redux state

--- src/pages/beer/Beer.js
+++ src/pages/beer/Beer.js
@@ -3,30 +3,45 @@ import {
   useParams,
   Link
 } from "react-router-dom";
+
+import { connect } from "react-redux";
+
 import Loader from "react-loader-spinner";
 import BeerDetails from '../../components/beerDetails/BeerDetails';
-import api from '../../api';
 import './Beer.css';
 import Button from '../../components/button/Button';
 import { BiArrowBack } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
 
-function Beer() {
+function Beer(props) {
   let {id} = useParams();
-  const [beer, setBeer] = useState("");
-  const [spinnerLoading, setSpinnerLoading] = useState(true);
-  const [hasError, setError] = useState(false);
+  // const [beer, setBeer] = useState("");
+  // const [spinnerLoading, setSpinnerLoading] = useState(true);
+  // const [hasError, setError] = useState(false);
 
-  useEffect(() => {
-    api.get(`/${id}`)
-    .then(response => {
-      setBeer(response.data[0]);  
-      setSpinnerLoading(false)})
-      .catch(error => {
-        setError(true);
-        setSpinnerLoading(false);
-      })
-  }, []);
+  const {beers} = props;
+  console.log(beers)
+
+  const beer = beers.filter(function (item) {
+    return item.id == id;
+  });
+
+  console.log(beer)
+
+
+  // useEffect(() => {
+  //   getBeer(beers)
+
+  //   console.log(beer)
+  //   // api.get(`/${id}`)
+  //   // .then(response => {
+  //   //   setBeer(response.data[0]);  
+  //   //   setSpinnerLoading(false)})
+  //   //   .catch(error => {
+  //   //     setError(true);
+  //   //     setSpinnerLoading(false);
+  //   //   })
+  // }, [beers, id]);
   
   return (
     <>
@@ -34,13 +49,22 @@ function Beer() {
         <Link to="/table"><span><BiArrowBack /> back to the list</span></Link>
         <Button label="add to the favorite" icon={<FaRegHeart />}/>
       </div>
-        {hasError && <p>An error has occurred, try later</p>}
+        {props.isError && <p>An error has occurred, try later</p>}
         {
-          spinnerLoading ? <Loader type="ThreeDots" color="#31357F" height={100} width={100} /> : 
-          <BeerDetails data={beer}/>
+          props.isLoading ? <Loader type="ThreeDots" color="#31357F" height={100} width={100} /> : 
+          <BeerDetails data={beer[0]}/>
         }
     </>
   );
 }
 
-export default Beer;
+const mapStateToProps = (state) => {
+  return {
+    beers: state.beers.beers,
+    isLoading: state.beers.isLoading,
+    isError: state.beers.isError
+  };
+};
+
+
+export default connect(mapStateToProps)(Beer);
