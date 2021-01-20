572895dd2a5405a2af86c80568ace39160509b12
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-01-20 13:56:19 +0100

    add redux-persist to favorite

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import {
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+import { PersistGate } from 'redux-persist/integration/react'
 
 import rootReducer from "./rootReducer";
 import Nav from './components/navigation/Nav';
@@ -17,32 +20,43 @@ import Random from './pages/random/Random';
 import Beers from './pages/beers/containers/Beers';
 import FavoriteContainer from './pages/favorite/components/FavoriteContainer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['favorite'] 
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store)
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Router>
-          <Nav />
-          <Switch>
-            <Route path="/beers/:id">
-              <Beer />
-            </Route>
-            <Route path="/beers">
-              <Beers />
-            </Route>
-            <Route path="/random">
-              <Random />
-            </Route>
-            <Route path="/favorite">
-              <FavoriteContainer />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Nav />
+            <Switch>
+              <Route path="/beers/:id">
+                <Beer />
+              </Route>
+              <Route path="/beers">
+                <Beers />
+              </Route>
+              <Route path="/random">
+                <Random />
+              </Route>
+              <Route path="/favorite">
+                <FavoriteContainer />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+        </PersistGate>
+        
       </Provider>
       
     </div>
